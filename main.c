1f13394d9210542aabffe7a8768e4e07773b787b
Tello-P <perezgarrote.tello@gmail.com>
2025-02-07 18:15:52 +0100

    Varias celulas salen pero ni se mueven ni cambian al volver a ejecutar

--- main.c
+++ main.c
@@ -3,18 +3,20 @@
 
 Uint32 COLOR_WHITE = 0xffffffff;
 Uint32 COLOR_GRAY = 0x2f2f2f2f;
+Uint32 COLOR_BLACK = 0x00000000;
 int SURFACE_WIDTH = 900;
 int SURFACE_HEIGHT = 600;
 int CELL_WIDTH = 30;
 int LINE_WIDTH = 2;
 
-int draw_cell(SDL_Surface* surface, int cell_x, int cell_y)
+int draw_cell(SDL_Surface* surface, int cell_x, int cell_y, int cell_value)
 {
 	int pixel_x = cell_x * CELL_WIDTH;
 	int pixel_y = cell_y * CELL_WIDTH;
+	Uint32 color = cell_value == 0 ? COLOR_BLACK : COLOR_WHITE;
 
 	SDL_Rect cell_rect = (SDL_Rect ){pixel_x, pixel_y, CELL_WIDTH,CELL_WIDTH};
-	SDL_FillRect(surface, &cell_rect, COLOR_WHITE);
+	SDL_FillRect(surface, &cell_rect, color);
 }
 
 int draw_grid(SDL_Surface* surface, int columns, int rows)
@@ -32,6 +34,34 @@ int draw_grid(SDL_Surface* surface, int columns, int rows)
 	}
 }
 
+void draw_game_matrix(SDL_Surface* surface, int rows, int columns, int* game_matrix)
+{
+	for(int i=0;i<rows;i++)
+	{
+		for(int j=0;j<columns;j++)
+		{
+			int cell_value = *(game_matrix + i*rows + j);
+			draw_cell(surface, i, j, cell_value);
+		}
+
+	}
+}
+
+
+
+void initialize_game_matrix(int rows, int columns, int *game_matrix)
+{
+	for(int i=0;i<rows;i++)
+	{
+		for(int j=0;j<columns;j++)
+		{
+			*(game_matrix + i*rows + j) = rand() % 2;
+		}
+	}
+}
+
+
+
 int main()
 {
 	printf("Hola mundo\n");
@@ -47,17 +77,16 @@ int main()
 	
 	SDL_Surface* surface = SDL_GetWindowSurface(window);
 
-	
+	int row_count = SURFACE_HEIGHT / CELL_WIDTH;
+	int column_count = SURFACE_WIDTH / CELL_WIDTH;
+	int game_matrix[row_count][column_count];
+	initialize_game_matrix(row_count,column_count,game_matrix[0]);
+
 	int cell_x = 10;
 	int cell_y = 6;
 
-	draw_cell(surface, cell_x, cell_y);
-	draw_cell(surface, 10, 7);
-	draw_cell(surface, 11, 9);
-	draw_cell(surface, 15, 9);
-	draw_cell(surface, 8, 4);
+	draw_game_matrix(surface, row_count, column_count, game_matrix[0]);
 	draw_grid(surface, columns, rows);
-
 	SDL_UpdateWindowSurface(window);
 	SDL_Delay(5000);
 	
